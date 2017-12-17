d581e4e51c98ed1ddf153a400f199860bae64696
Dragan Ljubojevic <ljufa@yahoo.com>
2017-12-17 21:40:16 +0100

    add dummy junit test for springboot config

---
+++ resultcapture-service/src/test/kotlin/com/devotion/survey/DummyApplicationTests.kt
@@ -0,0 +1,19 @@
+package com.devotion.survey
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+import org.springframework.test.context.web.WebAppConfiguration
+
+@RunWith(SpringJUnit4ClassRunner::class)
+@SpringBootApplication
+@WebAppConfiguration
+class DummyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+
+    }
+
+}
