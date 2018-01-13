b8947d5f86ef9d102b97c9b7be82b9982f4b366a
Dusan Odalovic <dodalovic@gmail.com>
2018-01-13 14:29:40 +0100

    Create sendGenericMessage on KafkaTemplate | Some cleanup | More idiomatic kotlin

--- authoring-service/src/main/kotlin/com/devotion/authoring/service/DefaultSurveyAuthoringService.kt
+++ authoring-service/src/main/kotlin/com/devotion/authoring/service/DefaultSurveyAuthoringService.kt
@@ -9,41 +9,36 @@ import com.devotion.authoring.model.Survey
 import org.modelmapper.ModelMapper
 import org.modelmapper.TypeToken
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.support.GenericMessage
 import org.springframework.stereotype.Service
-import java.util.*
 import javax.validation.Valid
 import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 @Service
-class DefaultSurveyAuthoringService(@Autowired private val surveyRepository: SurveyRepository,
-                                    @Autowired private val answerRepository: AnswerRepository,
-                                    @Autowired private val modelMapper: ModelMapper,
-                                    @Autowired private val kafkaConfig: KafkaConfig,
-                                    @Autowired private val kafkaTemplate: KafkaTemplate<String, String>) : SurveyAuthoringService {
+class DefaultSurveyAuthoringService(private val surveyRepository: SurveyRepository,
+                                    private val answerRepository: AnswerRepository,
+                                    private val modelMapper: ModelMapper,
+                                    private val kafkaConfig: KafkaConfig,
+                                    private val kafkaTemplate: KafkaTemplate<String, String>) : SurveyAuthoringService {
 
     private val log = LoggerFactory.getLogger(DefaultSurveyAuthoringService::class.java)
     private val questionDtoListType = object : TypeToken<List<QuestionIdAndText>>() {}.type
     private val answersDtoListType = object : TypeToken<List<AnswerIdAndText>>() {}.type
 
-    /**
-     * Write functions
-     */
     override fun addQuestion(@NotEmpty surveyId: String, @Valid question: QuestionText) {
-        kafkaTemplate.send(createGenericMessage(ModifyQuestionEvent(Action.CREATE, surveyId, null, question), kafkaConfig.questionCapturedTopic))
+        kafkaTemplate.sendGenericMessage(ModifyQuestionEvent(Action.CREATE, surveyId, null, question), kafkaConfig.questionCapturedTopic)
     }
 
     override fun updateQuestion(@NotEmpty surveyId: String, @NotNull questionId: String, @Valid question: QuestionText) {
-        kafkaTemplate.send(createGenericMessage(ModifyQuestionEvent(Action.UPDATE, surveyId, questionId, question), kafkaConfig.questionCapturedTopic))
+        kafkaTemplate.sendGenericMessage(ModifyQuestionEvent(Action.UPDATE, surveyId, questionId, question), kafkaConfig.questionCapturedTopic)
     }
 
     override fun deleteQuestion(@NotEmpty surveyId: String, @NotNull questionId: String) {
-        kafkaTemplate.send(createGenericMessage(ModifyQuestionEvent(Action.DELETE, surveyId, questionId), kafkaConfig.questionCapturedTopic))
+        kafkaTemplate.sendGenericMessage(ModifyQuestionEvent(Action.DELETE, surveyId, questionId), kafkaConfig.questionCapturedTopic)
     }
 
     @KafkaListener(topics = ["\${kafka.questionCapturedTopic}"], containerFactory = "jsonKafkaListenerContainerFactory", errorHandler = "validationErrorHandler")
@@ -58,34 +53,27 @@ class DefaultSurveyAuthoringService(@Autowired private val surveyRepository: Sur
                 survey.questions.add(newQuestion)
             }
             Action.UPDATE -> {
-                survey.questions.set(survey.questions.indexOfFirst { it.id == newQuestion.id }, newQuestion)
+                survey.questions[survey.questions.indexOfFirst { it.id == newQuestion.id }] = newQuestion
             }
             Action.DELETE -> {
-                survey.questions.removeAt(survey.questions.indexOfFirst { it.id == newQuestion.id });
+                survey.questions.removeAt(survey.questions.indexOfFirst { it.id == newQuestion.id })
             }
         }
         surveyRepository.save(survey)
-        kafkaTemplate.send(createGenericMessage(survey, kafkaConfig.surveyStoredTopic))
+        kafkaTemplate.sendGenericMessage(survey, kafkaConfig.surveyStoredTopic)
     }
 
     override fun addAnswer(@NotEmpty surveyId: String, @NotNull questionId: String, @Valid answer: AnswerText): Answer {
         validate(surveyId, questionId)
-        val newAnswer = modelMapper.map(answer, Answer::class.java)
-        newAnswer.surveyId = surveyId
-        return answerRepository.insert(newAnswer)
+        return answerRepository.insert(modelMapper.map(answer, Answer::class.java).apply { this.surveyId = surveyId })
     }
 
     override fun updateAnswer(@NotEmpty answerId: String, @Valid answer: AnswerText) {
-        val existingOne = answerRepository.findById(answerId).get()
-        existingOne.answerText = answer.answerText
         answerRepository.save(answerRepository.findById(answerId).get().apply { answerText = answer.answerText })
     }
 
     override fun deleteAnswer(@NotEmpty answerId: String) = answerRepository.deleteById(answerId)
 
-    /**
-     * Read functions
-     */
     override fun getQuestion(@NotEmpty surveyId: String, @NotNull questionId: String, fetchAnswers: Boolean): QuestionAll {
         log.debug("Invoke get question surveyId={}, questionId={}", surveyId, questionId)
         val survey = validate(surveyId, questionId)
@@ -101,11 +89,8 @@ class DefaultSurveyAuthoringService(@Autowired private val surveyRepository: Sur
         return questionDto
     }
 
-    override fun getAllQuestions(@NotEmpty surveyId: String): List<QuestionIdAndText> {
-        val survey = getValidSurvey(surveyId)
-        val questions = survey.questions
-        return modelMapper.map(questions, questionDtoListType)
-    }
+    override fun getAllQuestions(@NotEmpty surveyId: String): List<QuestionIdAndText> =
+            modelMapper.map(getValidSurvey(surveyId).questions, questionDtoListType)
 
     override fun getAllAnswers(@NotEmpty surveyId: String, @NotNull questionId: String): List<AnswerIdAndText> {
         validate(surveyId, questionId)
@@ -115,7 +100,7 @@ class DefaultSurveyAuthoringService(@Autowired private val surveyRepository: Sur
 
     // todo: implement better and more flexible/reusable validation by using spring custom validators
     private fun getValidSurvey(surveyId: String): Survey {
-        var input = surveyRepository.findById(surveyId)
+        val input = surveyRepository.findById(surveyId)
         if (!input.isPresent) {
             throw ValidationException("Survey [$input] could not be found")
         }
@@ -128,7 +113,6 @@ class DefaultSurveyAuthoringService(@Autowired private val surveyRepository: Sur
 
     private fun validate(surveyId: String, questionId: String): Survey {
         val survey = getValidSurvey(surveyId)
-        val questions = survey.questions
         if (survey.questions.filter { it.id == questionId }.size != 1) {
             throw ValidationException("Survey [$survey] has no question with id [$questionId]")
         }
@@ -162,6 +146,6 @@ class DefaultSurveyAuthoringService(@Autowired private val surveyRepository: Sur
         }
     }
 
-    private fun createGenericMessage(event: Any, topic: String) =
-            GenericMessage(event, mapOf<String, Any>(KafkaHeaders.TOPIC to topic))
+    private fun KafkaTemplate<String, String>.sendGenericMessage(event: Any, topic: String) =
+            send(GenericMessage(event, mapOf<String, Any>(KafkaHeaders.TOPIC to topic)))
 }
